2685cbee855a9912630ca44adb46f1728a46de5a
IllyaBudko <35385161+IllyaBudko@users.noreply.github.com>
2021-02-26 22:23:56 -0700

    commit

---
+++ rtos_mutex.c
@@ -0,0 +1,44 @@
+#include "RTOS_ish.h"
+#include "rtos_mutex.h"
+
+mutex_t * mutex_create(uint32_t init_state)
+{
+  mutex_t * mutex_ptr;
+  mutex_ptr->mutex_ptr = (uint32_t *)malloc(sizeof(mutex_t));
+  
+  mutex_ptr->lock_token = init_state;
+}
+
+uint32_t asm_set_mutex(mutex_t * mutex)
+{
+  uint32_t set_success = 0x01;
+  __asm(
+  
+  "MOV      R0            , #0x01  \n\t"
+  "LDREX    %[set_success], [%[mutex]]  \n\t"
+  "CMP      %[set_success], #0x00 \n\t"
+  "STREXEQ  %[set_success], R0, [%[mutex]]  \n\t"
+  "DMBEQ  \n\t"
+  :[set_success] "=r" (set_success), [mutex] "+r" (mutex)
+  :
+  :"memory", "R0"
+  );
+  return set_success == 0;
+}
+
+uint32_t asm_reset_mutex(mutex_t * mutex)
+{
+  uint32_t set_success = 0x01;
+  __asm(
+  
+  "MOV      R0            , #0x00  \n\t"
+  "LDREX    %[set_success], [%[mutex]]  \n\t"
+  "CMP      %[set_success], #0x01 \n\t"
+  "STREXEQ  %[set_success], R0, [%[mutex]]  \n\t"
+  "DMBEQ  \n\t"
+  :[set_success] "=r" (set_success), [mutex] "+r" (mutex)
+  :
+  :"memory", "R0"
+  );
+  return set_success == 0;
+}
---
+++ rtos_mutex.h
@@ -0,0 +1,17 @@
+#ifndef _RTOS_MUTEX_H_
+#define _RTOS_MUTEX_H_
+
+#include "RTOS_ish.h"
+
+typedef struct {
+  
+  uint32_t lock_token;
+  void * mutex_ptr;
+  
+}mutex_t;
+
+mutex_t * mutex_create(uint32_t init_state);
+uint32_t asm_set_mutex(mutex_t * mutex);
+uint32_t asm_reset_mutex(mutex_t * mutex);
+
+#endif /*_RTOS_MUTEX_H_*/
